cd6d755adfb62b58e6e886be0a51ab4a58b908a7
Nino <nino@turcin.me>
2022-07-18 13:41:14 +0200

    Refactor file creation/validation + function doc

--- main.go
+++ main.go
@@ -9,24 +9,25 @@ const filename = "secrets.json"
 
 func main() {
 	fileExists()
-
-	f, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Printf("could not read file: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(f))
 }
 
+// fileExists makes sure the file with the secrets exists, by creating it if it doesn't already.
+// If an error occurs with either reading or creating it, it outputs the error and exits the
+// program.
 func fileExists() {
-	_, err := os.Stat(filename)
+	// TODO: test: https://pkg.go.dev/testing/fstest
+	_, err := os.ReadFile(filename)
 	if os.IsNotExist(err) {
-		_, err := os.Create(filename)
+		_, err = os.Create(filename)
 
 		if err != nil {
 			fmt.Printf("failed to create file: %s\n", err)
 			os.Exit(1)
 		}
+	}
+
+	if err != nil {
+		fmt.Printf("could not read file: %s\n", err)
+		os.Exit(1)
 	}
 }
