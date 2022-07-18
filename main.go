c68723303ade7fe9e455908d1b10d9b3fd1dd8e1
Nino <nino@turcin.me>
2022-07-18 13:45:16 +0200

    Adds a Hello World server listening on port 8000

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -9,6 +11,7 @@ const filename = "secrets.json"
 
 func main() {
 	fileExists()
+	server()
 }
 
 // fileExists makes sure the file with the secrets exists, by creating it if it doesn't already.
@@ -30,4 +33,13 @@ func fileExists() {
 		fmt.Printf("could not read file: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+// server starts listening on all the endpoints and passes the calls to the handlers
+func server() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
