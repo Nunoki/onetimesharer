d4fab7a95f6bc3be7dcfc93a53d11e48f9f7742b
Nino <nino@turcin.me>
2022-07-21 07:59:07 +0200

    Add saving posted secret (+ ready methods for other actions)

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 const filename = "secrets.json"
+const appURL = "http://localhost"
+
+var errSecretNotFound = errors.New("secret not found")
 
 type tplData struct {
 	ShareURL  string
 	SecretKey string
 	ErrorMsg  string
 }
+
+type secrets map[string]string
 
 func main() {
 	verifyFile()
@@ -49,24 +59,61 @@ func serve() {
 	// TODO: test all endpoints
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
+			// this is because the "/" pattern of HandleFunc matches everything
 			http.NotFound(w, r)
 			return
 		}
 
 		if r.Method == "GET" {
 			handleIndex(w, r)
+		} else if r.Method == "POST" {
+			handlePost(w, r)
+			// TODO
+			// } else if r.Method == "DELETE" {
+			// 	handleDelete(w, r)
 		} else {
 			w.WriteHeader(http.StatusBadRequest)
 		}
 	})
 
+	// TODO
+	// http.HandleFunc("/show", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.Method == "GET" {
+	// 		handleShow(w, r)
+	// 	} else {
+	// 		w.WriteHeader(http.StatusBadRequest)
+	// 	}
+	// })
+
 	log.Print("Listening on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-// handleIndex serves the index.html page
+// handleIndex serves the default page for creating a new secret
 func handleIndex(w http.ResponseWriter, _ *http.Request) {
 	outputTpl(w, tplData{})
+}
+
+// handlePost stores the posted secret and outputs the generated key for reading it
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	secret := r.FormValue("secret")
+	if secret == "" {
+		http.Error(w, "failed to read posted content", http.StatusBadRequest)
+		return
+	}
+	log.Print("secret: ", secret)
+
+	key, err := saveSecret(secret)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "failed to save secret", http.StatusInternalServerError)
+		return
+	}
+
+	data := tplData{
+		ShareURL: appURL + "/show?key=" + key,
+	}
+	outputTpl(w, data)
 }
 
 // outputTpl parses the index.html file and outputs it to the w writer, passing the data to it
@@ -77,4 +124,75 @@ func outputTpl(w http.ResponseWriter, data tplData) {
 	if err != nil {
 		log.Print(err)
 	}
+}
+
+// DOCME
+func saveSecret(secret string) (string, error) {
+	// TODO encrypt
+	key := randStr(40)
+	secrets, err := readAllSecrets()
+	if err != nil {
+		return "", err
+	}
+
+	secrets[key] = string(secret)
+
+	jsonData, err := json.Marshal(secrets)
+	if err != nil {
+		return "", err
+	}
+	os.WriteFile(filename, jsonData, os.FileMode(0111))
+
+	return key, nil
+}
+
+// DOCME
+func deleteSecret(key string) error {
+	secrets, err := readAllSecrets()
+	if err != nil {
+		return err
+	}
+
+	delete(secrets, key)
+	return nil
+}
+
+// DOCME
+func readSecret(key string) (string, error) {
+	secrets, err := readAllSecrets()
+	if err != nil {
+		return "", err
+	}
+	secret, ok := secrets[key]
+	if !ok {
+		return "", errSecretNotFound
+	}
+	return secret, nil
+}
+
+// DOCME
+func readAllSecrets() (secrets, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData := secrets{}
+	err = json.Unmarshal(content, &jsonData)
+	if err != nil {
+		return nil, err
+	}
+	return jsonData, nil
+}
+
+// randStr returns a random string of length n
+func randStr(n int) string {
+	rand.Seed(time.Now().UnixMilli())
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }
