6d82fab2913319a9b0cdcc5787f825d66f5741e9
Nino <nino@turcin.me>
2022-07-31 17:17:41 +0200

    Document Crypter interface

--- internal/pkg/crypter/crypter.go
+++ internal/pkg/crypter/crypter.go
@@ -1,5 +1,8 @@
 package crypter
 
+// Crypter is implemented by any value that has an Encrypt() and Decrypt() method. It is used for
+// encryption of secrets and their keys before passing it to the storage medium, and corresponding
+// decryption when reading it back.
 type Crypter interface {
 	Decrypt(encrypted string) (string, error)
 	Encrypt(plaintext string) (string, error)
